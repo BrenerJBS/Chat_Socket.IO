7ea7f044f7d4aa823b1632d7045e448ad0e8aaf0
BrenerJBS <brenerjoabe@gmail.com>
2022-03-13 07:41:56 -0300

    Chat server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const INDEX = '/index.html';
 const app = express()
 app.use(cors())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://onledu.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
